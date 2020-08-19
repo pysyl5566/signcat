5d230d34a7447cd7d62e523c7d4ccb897bb1ef35
pysyl5566 <henanpysyl@126.com>
2020-08-19 13:15:21 +0800

    本地初始化。

---
+++ src/main/java/top/nextcat/SignCat/SignCatApplication.java
@@ -0,0 +1,13 @@
+package top.nextcat.SignCat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/nextcat/SignCat/SignCatApplicationTests.java
@@ -0,0 +1,13 @@
+package top.nextcat.SignCat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
